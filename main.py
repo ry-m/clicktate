19ac3eadbb0858942c20c191fdc3d6a159c6157f
ryan <rkm1230@gmail.com>
2022-01-31 16:03:50 +0800

    Game window and basic loop

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-# This is a sample Python script.
+import pygame
+
+
+# Constants.
+SIZE = (800, 600)
+BG_COLOUR = (210, 255, 120)
 
 
 def main():
-    print('Hello, world!')
+    """
+    Main program loop.
+    """
+    pygame.init()
+    screen = pygame.display.set_mode(SIZE)
+    pygame.display.set_caption('Clicktate')
+
+    running = True
+
+    while running:
+        # Handle events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Rendering.
+        screen.fill(BG_COLOUR)
+        pygame.display.flip()
+
+    pygame.quit()
 
 
 if __name__ == '__main__':
